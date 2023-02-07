a48fe08ffc2bc3e99ce169a3cccd94030f760800
DanielPFM01 <daniel.pfm01@gmail.com>
2023-02-07 18:11:44 -0300

    [UPDATE] Atualização da Árvore Rubro-Negra

--- src/ArvRB/Main.java
+++ src/ArvRB/Main.java
@@ -1,28 +1,70 @@
 package ArvRB;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Arvore arvoreRB = new Arvore();
+        //create instance of the Scanner class
+        Scanner scan = new Scanner(System.in);
 
-        arvoreRB.adiciona(13);
-        arvoreRB.adiciona(67);
-        arvoreRB.adiciona(91);
-        arvoreRB.adiciona(38);
-        arvoreRB.adiciona(7);
-        arvoreRB.adiciona(1);
+        //Create instance of the createRedBlackTree class and pass minimum integer value to the constructor to make it header node
+        CreateRedBlackTree obj = new CreateRedBlackTree(Integer.MIN_VALUE);
+        char choice;
 
-        //arvoreRB.grafico();
+        //create switch case for performing the operation in Red Black Tree
+        do
+        {
+            System.out.println("\nSelect an operation:\n");
+            System.out.println("1. Insert a node ");
+            System.out.println("2. Search a node");
+            System.out.println("3. Get total number of nodes in Red Black Tree");
+            System.out.println("4. Is Red Black Tree empty?");
+            System.out.println("5. Remove all nodes from Red Black Tree");
+            System.out.println("6. Display Red Black Tree in Post order");
+            System.out.println("7. Display Red Black Tree in Pre order");
+            System.out.println("8. Display Red Black Tree in In order");
 
-        System.out.println( "O menor valor da arvore e: " + arvoreRB.menorValor());
-
-        System.out.println( "O maior valor da arvore e: " + arvoreRB.maiorValor());
-
-//        arvoreRB.remove(38);
-//
-//        arvoreRB.grafico();
-//
-//        arvoreRB.inorderWalk();
-
+            //get choice from user
+            int ch = scan.nextInt();
+            switch (ch)
+            {
+                case 1 :
+                    System.out.println("Please enter an element to insert in Red Black Tree");
+                    obj.insertNewNode( scan.nextInt() );
+                    break;
+                case 2 :
+                    System.out.println("Enter integer element to search");
+                    System.out.println(obj.searchNode( scan.nextInt() ));
+                    break;
+                case 3 :
+                    System.out.println(obj.nodesInTree());
+                    break;
+                case 4 :
+                    System.out.println(obj.checkEmpty());
+                    break;
+                case 5 :
+                    obj.removeAll();
+                    System.out.println("\nTree Cleared successfully");
+                    break;
+                case 6 :
+                    System.out.println("\nDisplay Red Black Tree in Post order");
+                    obj.postorderTraversal();
+                    break;
+                case 7 :
+                    System.out.println("\nDisplay Red Black Tree in Pre order");
+                    obj.preorderTraversal();
+                    break;
+                case 8 :
+                    System.out.println("\nDisplay Red Black Tree in In order");
+                    obj.inorderTraversal();
+                    break;
+                default :
+                    System.out.println("\n ");
+                    break;
+            }
+            System.out.println("\nPress 'y' or 'Y' to continue \n");
+            choice = scan.next().charAt(0);
+        } while (choice == 'Y'|| choice == 'y');
     }
 }
